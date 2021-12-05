26fa9b3304609326133ce12249c193f74219761e
mehir narayana <mehirkn@gmail.com>
2021-12-05 15:43:55 -0500

    Created an instance called exampleMotor of VictorSp and created a method called setMtoro which sets the speed of the exampleMotor instance. Created an instance of the DriveTrain subsystem called driveTrain in Robot.java and called setMotor in autonomousPeriodic.

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.VictorSP;
+import frc.robot.RobotMap;
+
+ 
+/** Add your docs here. */
+public class DriveTrain extends Subsystem {
+  VictorSP exampleMotor = new VictorSP(RobotMap.Example_Motor_ID);  
+   
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void setMotor(){
+    exampleMotor.set(1);
+  }
+
+
+}
